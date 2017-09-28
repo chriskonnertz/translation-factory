646ceeac36baefb59cd13b5fc4b1722615210c72
ChrisKonnertz <siconize@gmail.com>
2017-09-28 14:48:31 +0200

    Added service provider stub

---
+++ src/ChrisKonnertz/TranslationFactory/Integration/TranslationFactoryServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ChrisKonnertz\DeepLy\Integrations\Laravel;
+
+use ChrisKonnertz\DeepLy\DeepLy;
+use Illuminate\Support\ServiceProvider;
+
+class TranslationFactoryServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+        /*
+        $this->app->bind('translation-factory', function()
+        {
+            return new ...();
+        });
+        */
+    }
+
+}
