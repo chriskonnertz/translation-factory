104a3d13418f895afda0dbb6e5ff51bc38de5ae4
ChrisKonnertz <siconize@gmail.com>
2017-09-29 23:16:37 +0200

    Ensure that the config file has been published

--- src/ChrisKonnertz/TranslationFactory/Controllers/TranslationFactoryController.php
+++ src/ChrisKonnertz/TranslationFactory/Controllers/TranslationFactoryController.php
@@ -6,8 +6,24 @@ use Illuminate\Routing\Controller as BaseController;
 
 class TranslationFactoryController extends BaseController
 {
-    public function index()
+
+    /**
+     * @param \Illuminate\Config\Repository $config
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Exception
+     */
+    public function index(\Illuminate\Config\Repository $config)
     {
+        // TODO Decide if this is a good idea
+        if ($config->get('translation-factory.user_authentication') === null) {
+            throw new \Exception(
+                'Please publish the assets of the Translation Factory package via: '.
+                '"php artisan vendor:publish '.
+                '--provider="ChrisKonnertz\TranslationFactory\Integration\TranslationFactoryServiceProvider"'
+            );
+        }
+
         return view('translationFactory::page_base');
     }
+
 }
