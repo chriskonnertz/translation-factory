f599812b960870e1979a841c5f96ffec57f84355
ChrisKonnertz <siconize@gmail.com>
2017-09-30 20:47:25 +0200

    Added reader/writer interface stub

---
+++ src/ChrisKonnertz/TranslationFactory/IO/TranslationReaderInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Chris
+ * Date: 30.09.2017
+ * Time: 20:39
+ */
+
+namespace ChrisKonnertz\TranslationFactory\IO;
+
+interface TranslationReaderInterface
+{
+    public function readAll();
+}
---
+++ src/ChrisKonnertz/TranslationFactory/IO/TranslationWriterInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Chris
+ * Date: 30.09.2017
+ * Time: 20:40
+ */
+
+namespace ChrisKonnertz\TranslationFactory\IO;
+
+interface TranslationWriterInterface
+{
+    public function writeAll();
+}
